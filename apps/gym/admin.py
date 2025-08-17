feee17001dcc6eb5db878fbe1a0b8b3411fe3894
mohammadsadeghian <133510008+MohammadSadeghiyan@users.noreply.github.com>
2025-08-17 16:59:59 +0330

    delete gym app
    don't need to gym app because features of gym is supported by reports app

--- apps/gym/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Gym
-# Register your models here.
-admin.site.register(Gym)
--- apps/gym/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.core.validators import MinValueValidator
-# Create your models here.
-
-class Gym(models.Model):
-    input_salary=models.DecimalField(max_digits=18,decimal_places=0,validators=[MinValueValidator(0)])
-    ouput_salary=models.DecimalField(max_digits=18,decimal_places=0,validators=[MinValueValidator(0)])
