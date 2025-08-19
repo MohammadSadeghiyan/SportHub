3d6824cd2ab2851fd15a9a84da31e46e0c23aed8
mohammadsadeghian <133510008+MohammadSadeghiyan@users.noreply.github.com>
2025-08-19 20:35:21 +0330

    add public_id lookup for receptionist app
    check athlete app -> it's ok
    ckeck receptionists app -> its make error

--- apps/receptionists/views.py
+++ apps/receptionists/views.py
@@ -10,9 +10,10 @@ class ReceptionistViewSet(viewsets.ModelViewSet):
         user=self.request.user
         if user.role=='manager':
             return Receptionist.objects.all()
-        return Receptionist.objects.get(pk=user.pk)
+        return Receptionist.objects.get(public_id=user.public_id)
     serializer_class=ReceptionistSerializer
     permission_classes=[permissions.IsAuthenticated,IsManagerOrRecptionist]
+    lookup_field='public_id'
 
     
 
